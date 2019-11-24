b1b21dc9c05518f374dfd0c86c88122921558a1e
Phillips <Tyler.Phillips@insight.com>
2019-11-24 13:19:56 -0500

    Expand options configuration

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,12 @@
 declare module 'sequelize-temporal' { 
+	type BaseNameSource = 'MODEL' | 'TABLE' | 'OPTIONS';
+	
 	interface Options { 
-		blocking?:boolean,
-		full?:boolean
+		blocking?: boolean,
+		full?: boolean,
+		modelSuffix?: string,
+		tableSuffix?: string,
+		baseNameSource?: BaseNameSource
 	}
 
 	function output<T>(define:T, sequelize:any, options?:Options): T
